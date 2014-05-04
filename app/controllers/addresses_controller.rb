f8d6bcdd561e25be17574ccb3777c3f1d839ff9e
alex greenwood <alexander.greenwood@gmail.com>
2014-05-04 17:29:45 -0500

    updated my typo in the app controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -10,7 +10,7 @@ class AddressesController < ApplicationController
     @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
 
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+foster+and+sheridan&sensor=false"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
 
