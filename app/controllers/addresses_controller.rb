7470fa90199b55f1f23debe2563b82c76fb1a3cd
alex greenwood <alexander.greenwood@gmail.com>
2014-05-04 18:09:07 -0500

    fixed the order in addresses controller
    it works!

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -6,14 +6,12 @@ class AddressesController < ApplicationController
     @address = "the corner of Foster and Sheridan"
     @url_safe_address = URI.encode(@address)
 
-    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
-    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
-
 
     url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+foster+and+sheridan&sensor=false"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
-
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
   end
 end
