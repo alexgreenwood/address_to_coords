4bb7572f0533ccb592f1c446ebc6c72e9e31c594
alex greenwood <alexander.greenwood@gmail.com>
2014-05-04 18:17:48 -0500

    getting rid of the built in address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,7 +7,7 @@ class AddressesController < ApplicationController
     @url_safe_address = URI.encode(@address)
 
 
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+foster+and+sheridan&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=false"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
     @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
