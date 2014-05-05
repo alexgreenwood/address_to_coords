68388745a36dae47efcdfba9eae2552e061f96e9
alex greenwood <alexander.greenwood@gmail.com>
2014-05-04 21:18:24 -0500

    phase 2 final
    made /weather show up without an address entered

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,15 +3,29 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
-    @url_safe_address = URI.encode(@address)
 
+
+if
+
+    @address = params[:address]
+
+
+    @url_safe_address = URI.encode(@address)
 
     url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=false"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
     @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
     @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+else
+
+@address != params[:address]
+
+
+    @latitude = " "
+    @longitude = " "
+
 
   end
+end
 end
